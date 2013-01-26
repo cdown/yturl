89ca47e19659a4c79d874699d1e03e5f6e38f9ba
Christopher Down <chris@regentmarkets.com>
2013-01-26 20:42:53 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = "yturl",
+    version = "20130126",
+    description = "A simple media URL grabber for YouTube.",
+    author = "Chris Down",
+    author_email = "chris@illco.de",
+    url = "http://illco.de",
+    scripts = ( "yturl", )
+)
