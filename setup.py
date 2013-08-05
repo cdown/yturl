8ac9e82719ec0ef5bf850ea1d0c7840e7007d772
Chris Down <chris@chrisdown.name>
2013-08-05 18:51:59 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = "yturl",
+    version = "1.15.3",
+    description = "Prints direct URLs to YouTube videos.",
+    author = "Chris Down",
+    author_email = "chris@chrisdown.name",
+    url = "http://chrisdown.name",
+    scripts = [ "yturl" ],
+)
