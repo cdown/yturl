c7b22aac4a75eb576b037cad69ce85057efd0ec9
Chris Down <chris@chrisdown.name>
2013-08-21 23:03:57 +0200

    Update url/classifiers in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,22 @@ setup(
     name = "yturl",
     version = "1.15.4",
     description = "Prints direct URLs to YouTube videos.",
+    url = "https://github.com/cdown/yturl",
+
     author = "Chris Down",
     author_email = "chris@chrisdown.name",
-    url = "http://chrisdown.name",
+
     scripts = [ "yturl" ],
+
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Multimedia",
+        "Topic :: Internet",
+        "Topic :: Utilities",
+    ],
 )
