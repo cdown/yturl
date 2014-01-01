96018479b182116418712dbbf61526c5709398cd
Chris Down <chris@chrisdown.name>
2014-01-01 15:01:57 +0800

    travis: Unsupport Python 2.6
    This would require me to care about 2.6 not having argparse as part of
    the standard library, which I really don't care about to be honest.

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
