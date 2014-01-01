4994b45c59a5cec12b77b86842e287cd642ac309
Chris Down <chris@chrisdown.name>
2014-01-01 16:58:33 +0800

    tests: Use a mock instead of reading from files directly

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python2
 
 import os
+import sys
 import yturl
 from nose.tools import assert_raises, eq_
+from mock import patch
 
 try:
     from urllib.request import urlopen
@@ -22,21 +24,26 @@ def test_url_stripping():
     eq_(yturl.strip_to_video_id("http://www.youtube.com/watch?feature=player_embedded&v=gEl6TXrkZnk"), "gEl6TXrkZnk")
     eq_(yturl.strip_to_video_id("youtu.be/gEl6TXrkZnk#foo"), "gEl6TXrkZnk")
 
-def test_available_itags_parsing():
-    with open(os.path.join(os.path.dirname(__file__), "api_output/good"), "rb") as f:
-        avail = yturl.available_itags(None, f)
-        eq_(list(avail),
-            [('43',
-              'http://r1---sn-uh-2xol.c.youtube.com/videoplayback?mv=m&ratebypass=yes&source=youtube&ms=au&itag=43&sparams=cp%2Cid%2Cip%2Cipbits%2Citag%2Cratebypass%2Csource%2Cupn%2Cexpire&cp=U0hVSlZLU19HUENONV9ORVdBOlZjeTVCdFBGbVRx&upn=c0mR0CEwjLA&key=yt1&id=2b16823874f4a662&mt=1365582793&newshard=yes&ipbits=8&expire=1365605026&fexp=906373%2C912519%2C904828%2C911305%2C914072%2C916625%2C913813%2C932000%2C932004%2C906383%2C916910%2C902000%2C901208%2C919512%2C929903%2C925714%2C931202%2C900821%2C900823%2C931203%2C931401%2C906090%2C909419%2C908529%2C904830%2C930807%2C919373%2C930803%2C906836%2C920201%2C929602%2C930101%2C900824%2C910223&ip=175.136.239.229&sver=3&signature=CF8569C59EA5BA26893E056A143CE7198B63127F.CB324AEDF80381BA93DDC7E9FC7A1FB4BAF06F10'),
-             ('34',
-              'http://r1---sn-uh-2xol.c.youtube.com/videoplayback?ip=175.136.239.229&source=youtube&ms=au&itag=34&mv=m&sparams=algorithm%2Cburst%2Ccp%2Cfactor%2Cid%2Cip%2Cipbits%2Citag%2Csource%2Cupn%2Cexpire&cp=U0hVSlZLU19HUENONV9ORVdBOlZjeTVCdFBGbVRx&upn=c0mR0CEwjLA&algorithm=throttle-factor&burst=40&id=2b16823874f4a662&key=yt1&mt=1365582793&newshard=yes&fexp=906373%2C912519%2C904828%2C911305%2C914072%2C916625%2C913813%2C932000%2C932004%2C906383%2C916910%2C902000%2C901208%2C919512%2C929903%2C925714%2C931202%2C900821%2C900823%2C931203%2C931401%2C906090%2C909419%2C908529%2C904830%2C930807%2C919373%2C930803%2C906836%2C920201%2C929602%2C930101%2C900824%2C910223&expire=1365605026&ipbits=8&sver=3&factor=1.25&signature=CB0AABC138D50FD014CC9CE184207B07C584859A.9E2CDB03A977884E98A8D680338B3C6E3D750DA9'),
-             ('18',
-              'http://r1---sn-uh-2xol.c.youtube.com/videoplayback?mv=m&ratebypass=yes&source=youtube&ms=au&itag=18&sparams=cp%2Cid%2Cip%2Cipbits%2Citag%2Cratebypass%2Csource%2Cupn%2Cexpire&cp=U0hVSlZLU19HUENONV9ORVdBOlZjeTVCdFBGbVRx&upn=c0mR0CEwjLA&key=yt1&id=2b16823874f4a662&mt=1365582793&newshard=yes&ipbits=8&expire=1365605026&fexp=906373%2C912519%2C904828%2C911305%2C914072%2C916625%2C913813%2C932000%2C932004%2C906383%2C916910%2C902000%2C901208%2C919512%2C929903%2C925714%2C931202%2C900821%2C900823%2C931203%2C931401%2C906090%2C909419%2C908529%2C904830%2C930807%2C919373%2C930803%2C906836%2C920201%2C929602%2C930101%2C900824%2C910223&ip=175.136.239.229&sver=3&signature=307E2380C1BF7D8CE460662EE86F78C904BD89CD.9DAED1D7D04B700C0695E80A2CE629DD853DE453'),
-             ('5',
-              'http://r1---sn-uh-2xol.c.youtube.com/videoplayback?ip=175.136.239.229&source=youtube&ms=au&itag=5&mv=m&sparams=algorithm%2Cburst%2Ccp%2Cfactor%2Cid%2Cip%2Cipbits%2Citag%2Csource%2Cupn%2Cexpire&cp=U0hVSlZLU19HUENONV9ORVdBOlZjeTVCdFBGbVRx&upn=c0mR0CEwjLA&algorithm=throttle-factor&burst=40&id=2b16823874f4a662&key=yt1&mt=1365582793&newshard=yes&fexp=906373%2C912519%2C904828%2C911305%2C914072%2C916625%2C913813%2C932000%2C932004%2C906383%2C916910%2C902000%2C901208%2C919512%2C929903%2C925714%2C931202%2C900821%2C900823%2C931203%2C931401%2C906090%2C909419%2C908529%2C904830%2C930807%2C919373%2C930803%2C906836%2C920201%2C929602%2C930101%2C900824%2C910223&expire=1365605026&ipbits=8&sver=3&factor=1.25&signature=30725A5BE0FE42CE6F9503D3F6C9AAAB23800F58.48C91855BAE589DA4749E48AAC067B89C7A7823D'),
-             ('17',
-              'http://r1---sn-uh-2xol.c.youtube.com/videoplayback?ip=175.136.239.229&source=youtube&ms=au&itag=17&mv=m&sparams=algorithm%2Cburst%2Ccp%2Cfactor%2Cid%2Cip%2Cipbits%2Citag%2Csource%2Cupn%2Cexpire&cp=U0hVSlZLU19HUENONV9ORVdBOlZjeTVCdFBGbVRx&upn=c0mR0CEwjLA&algorithm=throttle-factor&burst=40&id=2b16823874f4a662&key=yt1&mt=1365582793&newshard=yes&fexp=906373%2C912519%2C904828%2C911305%2C914072%2C916625%2C913813%2C932000%2C932004%2C906383%2C916910%2C902000%2C901208%2C919512%2C929903%2C925714%2C931202%2C900821%2C900823%2C931203%2C931401%2C906090%2C909419%2C908529%2C904830%2C930807%2C919373%2C930803%2C906836%2C920201%2C929602%2C930101%2C900824%2C910223&expire=1365605026&ipbits=8&sver=3&factor=1.25&signature=B4BEF7E233DA97A8BCB5EDBDA337BE3AAD3C9AAA.51924747D449A7CB28D6321AD6BBBCE12962042B')]
-        )
+@patch("yturl.urlopen")
+def test_available_itags_parsing(urlopen_mock):
+    f = open(os.path.join(os.path.dirname(__file__), "api_output/good"), "rb")
+    urlopen_mock.return_value = f
+
+    avail = yturl.available_itags("fake")
+    eq_(list(avail),
+        [('43',
+          'http://r1---sn-uh-2xol.c.youtube.com/videoplayback?mv=m&ratebypass=yes&source=youtube&ms=au&itag=43&sparams=cp%2Cid%2Cip%2Cipbits%2Citag%2Cratebypass%2Csource%2Cupn%2Cexpire&cp=U0hVSlZLU19HUENONV9ORVdBOlZjeTVCdFBGbVRx&upn=c0mR0CEwjLA&key=yt1&id=2b16823874f4a662&mt=1365582793&newshard=yes&ipbits=8&expire=1365605026&fexp=906373%2C912519%2C904828%2C911305%2C914072%2C916625%2C913813%2C932000%2C932004%2C906383%2C916910%2C902000%2C901208%2C919512%2C929903%2C925714%2C931202%2C900821%2C900823%2C931203%2C931401%2C906090%2C909419%2C908529%2C904830%2C930807%2C919373%2C930803%2C906836%2C920201%2C929602%2C930101%2C900824%2C910223&ip=175.136.239.229&sver=3&signature=CF8569C59EA5BA26893E056A143CE7198B63127F.CB324AEDF80381BA93DDC7E9FC7A1FB4BAF06F10'),
+         ('34',
+          'http://r1---sn-uh-2xol.c.youtube.com/videoplayback?ip=175.136.239.229&source=youtube&ms=au&itag=34&mv=m&sparams=algorithm%2Cburst%2Ccp%2Cfactor%2Cid%2Cip%2Cipbits%2Citag%2Csource%2Cupn%2Cexpire&cp=U0hVSlZLU19HUENONV9ORVdBOlZjeTVCdFBGbVRx&upn=c0mR0CEwjLA&algorithm=throttle-factor&burst=40&id=2b16823874f4a662&key=yt1&mt=1365582793&newshard=yes&fexp=906373%2C912519%2C904828%2C911305%2C914072%2C916625%2C913813%2C932000%2C932004%2C906383%2C916910%2C902000%2C901208%2C919512%2C929903%2C925714%2C931202%2C900821%2C900823%2C931203%2C931401%2C906090%2C909419%2C908529%2C904830%2C930807%2C919373%2C930803%2C906836%2C920201%2C929602%2C930101%2C900824%2C910223&expire=1365605026&ipbits=8&sver=3&factor=1.25&signature=CB0AABC138D50FD014CC9CE184207B07C584859A.9E2CDB03A977884E98A8D680338B3C6E3D750DA9'),
+         ('18',
+          'http://r1---sn-uh-2xol.c.youtube.com/videoplayback?mv=m&ratebypass=yes&source=youtube&ms=au&itag=18&sparams=cp%2Cid%2Cip%2Cipbits%2Citag%2Cratebypass%2Csource%2Cupn%2Cexpire&cp=U0hVSlZLU19HUENONV9ORVdBOlZjeTVCdFBGbVRx&upn=c0mR0CEwjLA&key=yt1&id=2b16823874f4a662&mt=1365582793&newshard=yes&ipbits=8&expire=1365605026&fexp=906373%2C912519%2C904828%2C911305%2C914072%2C916625%2C913813%2C932000%2C932004%2C906383%2C916910%2C902000%2C901208%2C919512%2C929903%2C925714%2C931202%2C900821%2C900823%2C931203%2C931401%2C906090%2C909419%2C908529%2C904830%2C930807%2C919373%2C930803%2C906836%2C920201%2C929602%2C930101%2C900824%2C910223&ip=175.136.239.229&sver=3&signature=307E2380C1BF7D8CE460662EE86F78C904BD89CD.9DAED1D7D04B700C0695E80A2CE629DD853DE453'),
+         ('5',
+          'http://r1---sn-uh-2xol.c.youtube.com/videoplayback?ip=175.136.239.229&source=youtube&ms=au&itag=5&mv=m&sparams=algorithm%2Cburst%2Ccp%2Cfactor%2Cid%2Cip%2Cipbits%2Citag%2Csource%2Cupn%2Cexpire&cp=U0hVSlZLU19HUENONV9ORVdBOlZjeTVCdFBGbVRx&upn=c0mR0CEwjLA&algorithm=throttle-factor&burst=40&id=2b16823874f4a662&key=yt1&mt=1365582793&newshard=yes&fexp=906373%2C912519%2C904828%2C911305%2C914072%2C916625%2C913813%2C932000%2C932004%2C906383%2C916910%2C902000%2C901208%2C919512%2C929903%2C925714%2C931202%2C900821%2C900823%2C931203%2C931401%2C906090%2C909419%2C908529%2C904830%2C930807%2C919373%2C930803%2C906836%2C920201%2C929602%2C930101%2C900824%2C910223&expire=1365605026&ipbits=8&sver=3&factor=1.25&signature=30725A5BE0FE42CE6F9503D3F6C9AAAB23800F58.48C91855BAE589DA4749E48AAC067B89C7A7823D'),
+         ('17',
+          'http://r1---sn-uh-2xol.c.youtube.com/videoplayback?ip=175.136.239.229&source=youtube&ms=au&itag=17&mv=m&sparams=algorithm%2Cburst%2Ccp%2Cfactor%2Cid%2Cip%2Cipbits%2Citag%2Csource%2Cupn%2Cexpire&cp=U0hVSlZLU19HUENONV9ORVdBOlZjeTVCdFBGbVRx&upn=c0mR0CEwjLA&algorithm=throttle-factor&burst=40&id=2b16823874f4a662&key=yt1&mt=1365582793&newshard=yes&fexp=906373%2C912519%2C904828%2C911305%2C914072%2C916625%2C913813%2C932000%2C932004%2C906383%2C916910%2C902000%2C901208%2C919512%2C929903%2C925714%2C931202%2C900821%2C900823%2C931203%2C931401%2C906090%2C909419%2C908529%2C904830%2C930807%2C919373%2C930803%2C906836%2C920201%2C929602%2C930101%2C900824%2C910223&expire=1365605026&ipbits=8&sver=3&factor=1.25&signature=B4BEF7E233DA97A8BCB5EDBDA337BE3AAD3C9AAA.51924747D449A7CB28D6321AD6BBBCE12962042B')]
+    )
+
+    f.close()
 
 def test_quality_group_parsing():
     parse = yturl.quality_group
@@ -46,7 +53,12 @@ def test_quality_group_parsing():
     eq_(known_itags.index(parse("medium", known_itags)), len(known_itags) // 2)
     eq_(known_itags.index(parse("low", known_itags)), len(known_itags) - 1)
 
-def test_embed_restriction_raises():
-    with open(os.path.join(os.path.dirname(__file__), "api_output/embed_restricted"), "rb") as f:
-        avail = yturl.available_itags(None, f)
-        assert_raises(LookupError, list, avail)
+@patch("yturl.urlopen")
+def test_embed_restriction_raises(urlopen_mock):
+    f = open(os.path.join(os.path.dirname(__file__), "api_output/embed_restricted"), "rb")
+    urlopen_mock.return_value = f
+
+    avail = yturl.available_itags("fake")
+    assert_raises(LookupError, list, avail)
+
+    f.close()
