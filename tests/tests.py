43e893de2c4bc5ccb12cd801f11cd8a802b76663
Chris Down <chris@chrisdown.name>
2014-06-11 14:10:30 +0800

    Normalise function naming

--- tests/tests.py
+++ tests/tests.py
@@ -11,25 +11,25 @@ try:
 except ImportError:  # Python 2 fallback
     from urllib import urlopen
 
-known_itags = yturl.default_itag_order()
+known_itags = yturl.itags_by_quality()
 
 def test_itag_order():
     eq_(known_itags, ['38', '37', '46', '22', '45', '44', '35', '43', '34', '18', '6', '5', '36', '17', '13'])
 
-def test_desired_itag_order():
-    itag_order = yturl.itag_order("18", known_itags)
-    eq_(list(itag_order), ['18', '6', '34', '5', '43', '36', '35', '17', '44', '13', '45', None, '22', None, '46', None, '37', None, '38', None])
+def test_desired_itags_by_similarity():
+    itags_by_similarity = yturl.itags_by_similarity("18", known_itags)
+    eq_(list(itags_by_similarity), ['18', '6', '34', '5', '43', '36', '35', '17', '44', '13', '45', None, '22', None, '46', None, '37', None, '38', None])
 
 def test_url_stripping():
-    eq_(yturl.strip_to_video_id("http://www.youtube.com/watch?feature=player_embedded&v=gEl6TXrkZnk"), "gEl6TXrkZnk")
-    eq_(yturl.strip_to_video_id("youtu.be/gEl6TXrkZnk#foo"), "gEl6TXrkZnk")
+    eq_(yturl.video_id_from_url("http://www.youtube.com/watch?feature=player_embedded&v=gEl6TXrkZnk"), "gEl6TXrkZnk")
+    eq_(yturl.video_id_from_url("youtu.be/gEl6TXrkZnk#foo"), "gEl6TXrkZnk")
 
 @patch("yturl.urlopen")
 def test_available_itags_parsing(urlopen_mock):
     f = open(os.path.join(os.path.dirname(__file__), "api_output/good"), "rb")
     urlopen_mock.return_value = f
 
-    avail = yturl.available_itags("fake")
+    avail = yturl.itags_for_video("fake")
     eq_(list(avail),
         [('43',
           'http://r1---sn-uh-30as.googlevideo.com/videoplayback?itag=43&ratebypass=yes&fexp=935640%2C943900%2C932261%2C945508%2C937417%2C913434%2C936910%2C936913%2C902907%2C934022&upn=7S8Or-4r_ts&key=yt5&ip=175.143.73.193&signature=4674C661CDD8B4A7D75537DC360B30FE9A0156C7.07375770BEA4BCC1BA7F99D1775D9DC5DD794CF0&mt=1394594747&expire=1394617426&id=2b16823874f4a662&sver=3&ms=au&ipbits=0&mv=m&sparams=id%2Cip%2Cipbits%2Citag%2Cratebypass%2Csource%2Cupn%2Cexpire&source=youtube'),
@@ -47,19 +47,17 @@ def test_available_itags_parsing(urlopen_mock):
     f.close()
 
 def test_quality_group_parsing():
-    parse = yturl.quality_group
-
-    eq_(parse("18", known_itags), "18")
-    eq_(known_itags.index(parse("high", known_itags)), 0)
-    eq_(known_itags.index(parse("medium", known_itags)), len(known_itags) // 2)
-    eq_(known_itags.index(parse("low", known_itags)), len(known_itags) - 1)
+    eq_(yturl.itag_from_quality("18", known_itags), "18")
+    eq_(known_itags.index(yturl.itag_from_quality("high", known_itags)), 0)
+    eq_(known_itags.index(yturl.itag_from_quality("medium", known_itags)), len(known_itags) // 2)
+    eq_(known_itags.index(yturl.itag_from_quality("low", known_itags)), len(known_itags) - 1)
 
 @patch("yturl.urlopen")
 def test_embed_restriction_raises(urlopen_mock):
     f = open(os.path.join(os.path.dirname(__file__), "api_output/embed_restricted"), "rb")
     urlopen_mock.return_value = f
 
-    avail = yturl.available_itags("fake")
+    avail = yturl.itags_for_video("fake")
     assert_raises(LookupError, list, avail)
 
     f.close()
